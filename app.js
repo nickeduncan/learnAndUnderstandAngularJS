d4d3767cc6d7eb09d8098774a56423895fef4f5f
Nick Duncan <nickeduncan@gmail.com>
2017-05-20 13:36:40 -0400

    used local and global namespace to restrict variable scope

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-// console.log($("h1").attr("reply"));
-// console.log($("h1").attr("data-reply"));
-console.log($("h1").attr("ng-reply"));
+var person = "Tony";
+
+// logPerson();
+
+stevesApp.person = person;
+stevesApp.logPerson();
---
+++ utility.js
@@ -0,0 +1,14 @@
+// var person = 'Tony';
+//
+// function logPerson()
+// {
+//     console.log(person);
+// }
+
+var stevesApp = {};
+stevesApp.person = 'Steve';
+
+stevesApp.logPerson = function()
+{
+    console.log(stevesApp.person);
+}
