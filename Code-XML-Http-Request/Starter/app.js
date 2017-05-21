df781c6dd15906001992c32ee2c2ef78ca103036
Nick Duncan <nickeduncan@gmail.com>
2017-05-21 17:12:27 -0400

    ran async XML http request

--- Code-XML-Http-Request/Starter/app.js
+++ Code-XML-Http-Request/Starter/app.js
@@ -1,23 +1,23 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
-    
+
     $scope.handle = '';
-    
+
     $scope.lowercasehandle = function() {
         return $filter('lowercase')($scope.handle);
     };
-    
+
     $scope.characters = 5;
-    
-    $scope.rules = [
-      
-        { rulename: "Must be 5 characters" },
-        { rulename: "Must not be used elsewhere" },
-        { rulename: "Must be cool" }
-        
-    ];
-    
-    console.log($scope.rules);
-    
+
+    var rulesrequest = new XMLHttpRequest();
+    rulesrequest.onreadystatechange = function() {
+        if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
+            $scope.rules = JSON.parse(rulesrequest.responseText);
+
+        }
+    }
+
+    rulesrequest.open("GET", "http://localhost:54765/api", true);
+    rulesrequest.send();
 }]);
