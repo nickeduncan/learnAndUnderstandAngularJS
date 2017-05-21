b5f18a04bfc89c7181462bb411977c7e1c69974f
Nick Duncan <nickeduncan@gmail.com>
2017-05-21 15:24:42 -0400

    used directive attributes to render different alerts on input rules

--- Code-Common-Directives/Starter/app.js
+++ Code-Common-Directives/Starter/app.js
@@ -1,11 +1,22 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
-    
+
     $scope.handle = '';
-    
+
     $scope.lowercasehandle = function() {
         return $filter('lowercase')($scope.handle);
     };
-    
+
+    $scope.characters = 5;
+
+    $scope.rules = [
+
+      { rulename: "Must be 5 characters" },
+      { rulename: "Must not be used elsewhere" },
+      { rulename: "Muse be cool" }
+
+    ];
+
+    console.log($scope.rules);
 }]);
