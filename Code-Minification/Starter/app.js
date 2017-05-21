5acef977f141c380141fe44b1ab10a2a939f5993
Nick Duncan <nickeduncan@gmail.com>
2017-05-21 12:29:38 -0400

    used arrays in dependency injection to make code minifiable

--- Code-Minification/Starter/app.js
+++ Code-Minification/Starter/app.js
@@ -1,5 +1,24 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope, $log) {
-    
-});
+// myApp.controller('mainController', function($scope, $log) {
+//
+//   $log.info($scope);
+//
+// });
+
+// myApp.controller("mainController",function(n,o){o.info(n)});
+
+// myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+//
+//   $log.info($scope);
+//
+// }]);
+
+// myApp.controller("mainController",["$scope","$log",function(o,n){n.info(o)}]);
+
+myApp.controller('mainController', ['$scope', '$log', function($log, $scope) {
+
+  // $log.info($scope);
+  console.log($scope);
+
+}]);
