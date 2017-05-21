da35511949421996211c1aa737632d380480b764
Nick Duncan <nickeduncan@gmail.com>
2017-05-21 12:02:33 -0400

    included messages and resources services to validate form

--- Code-Getting-Other-Services/Starter/app.js
+++ Code-Getting-Other-Services/Starter/app.js
@@ -1,5 +1,33 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope) {
-    
+// myApp.controller('mainController', function($scope, $log) {
+//
+//   console.log($scope);
+//   console.log($log);
+//
+// });
+
+// myApp.controller('mainController', function($log, $scope) {
+//
+//   console.log($scope);
+//   console.log($log);
+//
+// });
+
+myApp.controller('mainController', function($log, $scope, $filter, $resource) {
+
+  // $log.log("Hello");
+  // $log.info("This is some information.");
+  // $log.warn("Warning!");
+  // $log.debug("Some debug information while writing my code.");
+  // $log.error("This was an error!!");
+
+  // $scope.name = "John";
+  // $scope.formattedname = $filter('uppercase')($scope.name);
+  //
+  // $log.info($scope.name);
+  // $log.info($scope.formattedname);
+
+  console.log($resource);
+  
 });
